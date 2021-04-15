65394b43c2c4ede1f994141b417a425fd7689e01
Wale Adeniji <wale@Wale.DevOps>
2021-04-15 18:17:15 +0100

    added comment to the script

--- ssh/ssh_login.sh
+++ ssh/ssh_login.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+#MyUserName is the name of the user on the remote server/client
+#Replace 192.168.20.20 with the remote hostname or IP
 spawn ssh MyUserName@192.168.20.20
+
+#replace "password" with what's expected at the prompt after the previous command
 expect "password"
+
+#Replace "MyPassword" with the login password, remember to rns it with "\r"
+#This is not recmmended as the password is in clear text
 send "MyPassword\r"
 interact
